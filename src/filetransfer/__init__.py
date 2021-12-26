297f435cf2291cdf9ca3a6680b1303b2460f8be8
Andreas19 <andreas19@posteo.eu>
2021-12-26 10:03:40 +0100

    Bump version: 0.10.3 → 0.11.0

--- src/filetransfer/__init__.py
+++ src/filetransfer/__init__.py
@@ -16,7 +16,7 @@ from .exceptions import (Error, ConfigError, ConnectError, NotReadyError,
                          SingleInstanceError, TransferError, Terminated)
 from .job import JobResult
 
-__version__ = '0.10.3'
+__version__ = '0.11.0'
 
 __all__ = ['Error', 'ConfigError', 'ConnectError', 'TransferError',
            'SingleInstanceError', 'Terminated', 'NotReadyError', 'JobResult',
